c9a25dacb364358dc99e0fa959a15350e1ee2754
Gonzalo Saad <saad.gonzalo.ale@gmail.com>
2019-06-16 16:40:38 -0500

    Git por Fit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello fit");
         System.out.println("Hola Sameul");
     }
 }
