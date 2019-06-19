e760af6dca579a712821bddc0606afd20621c99c
Samuel <montiverosamuel@gmail.com>
2019-06-18 22:47:51 -0300

    Carpeta nueva y primer ejercicio de creacion de metodos

---
+++ src/week2MOOC/methodos/PrintText.java
@@ -0,0 +1,8 @@
+public class PrintText {
+    public static void main(String[] args) {
+        printText();
+    }
+    public static void printText(){
+        System.out.println("In the beginning there were the swamp, the hoe and Java.\n");
+    }
+}
