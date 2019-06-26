7ca31a954964cebf0271c469d108d0814c4f7c4b
Samuel <montiverosamuel@gmail.com>
2019-06-25 23:02:35 -0300

    leve avance sobre mtad izq triangulo, falta metodos en misma linea

--- src/week2MOOC/methodos/PrintLeftHalfTriangle.java
+++ src/week2MOOC/methodos/PrintLeftHalfTriangle.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class PrintLeftHalfTriangle {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        int triangulo = Integer.parseInt(reader.nextLine());
+        printLeftHalf(triangulo);
 
 
     }
@@ -11,6 +12,16 @@ public class PrintLeftHalfTriangle {
     public static void printLeftHalf(int alturaTriangulo){
     //buscar la forma de hacer que los dos metodos se usen en la misma linea
         // para formar la segunda mitad del triangulo
+        int stars = 1;
+        int espacios = (alturaTriangulo - 1);
+
+        while (stars <= alturaTriangulo){
+
+            printWhithespace(espacios);
+            printStars(stars);
+            stars ++;
+            espacios --;
+        }
 
     }
     public static void printWhithespace(int espacios){
