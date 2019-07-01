2aa5935de5cd19eb55fa2fda53ec04b4c29eef91
Samuel <montiverosamuel@gmail.com>
2019-06-30 23:49:53 -0300

    adivinar numeros listo, finalizada semana 2 de curso

--- src/week2MOOC/methodos/AdivinarNumero.java
+++ src/week2MOOC/methodos/AdivinarNumero.java
@@ -6,6 +6,25 @@ public class AdivinarNumero {
         Scanner reader = new Scanner(System.in);
         Random r = new Random();
         int numero = r.nextInt(100);
-        System.out.println(numero);
+        System.out.println("Adivine el numero:");
+        int guess = Integer.parseInt(reader.nextLine());
+        int intentos = 0;
+        while (!(guess == numero)){
+
+            if (guess < numero){
+                intentos ++;
+                System.out.println("El numero es mayor! Intentos: " + intentos);
+                System.out.println("Adivine el numero:");
+                guess = Integer.parseInt(reader.nextLine());
+            } else{
+                intentos ++;
+                System.out.println("El numero es menor! Intentos: " + intentos);
+                System.out.println("Adivine el numero:");
+                guess = Integer.parseInt(reader.nextLine());
+            }
+        }
+        intentos ++;
+        System.out.println("Correcto! Has dado en el clavo!" );
+        System.out.println("Te llevo " + intentos + " intentos!");
     }
 }
