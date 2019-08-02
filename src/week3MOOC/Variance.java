f137328cd168c2d709e561dbe9387d7eb5360595
Samuel <montiverosamuel@gmail.com>
2019-08-02 00:12:33 -0300

    Variance terminado, me costo refinarlo pero lo hice jaja sin embrago quedaron dudas pa mañana

--- src/week3MOOC/Variance.java
+++ src/week3MOOC/Variance.java
@@ -2,22 +2,38 @@ import java.util.ArrayList;
 
 public class Variance {
     public static double variance(ArrayList<Integer> lista){
+        ArrayList<Double> potencias = new ArrayList<Double>();
 
         for (int numero:lista) {
-            double potencias = Math.pow((numero + promedio(lista)), 2);
-
+            double resta = numero - promedio(lista);
+            double potenciaResta = Math.pow(resta, 2);
+            potencias.add(potenciaResta);
         }
+        double sumatoria = sumaPot(potencias);
+        System.out.println(sumatoria);
+        return sumatoria / (lista.size() - 1);
+
+
     }
-    public static int suma(ArrayList<Integer> list){
+    //Duda: hace falta hacer otro metodo para realizar la suma de doubles?
+    
+    public static double sumaPot(ArrayList<Double> list) {
+        double sum = 0;
+        for (double numero : list) {
+            sum = sum + numero;
+        }
+        return sum;
+    }
+    public static double suma(ArrayList<Integer> list){
         int sum = 0;
+        // duda ->   list.forEach((num) -> sum += num);
         for (int numero:list){
             sum = sum + numero;
         }
         return sum;
     }
-    public static double promedio(ArrayList<Integer> list){
-        double promedio = suma(list) / (list.size() * 1.0);
-        return promedio;
+    public static double promedio(ArrayList<Integer> list) {
+        return suma(list) / (((double)list.size()) * 1.0);
     }
 
     public static void main(String[] args) {
